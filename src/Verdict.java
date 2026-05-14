3a8480ac719e2dde3c40569a8e17f4a48c57ebe8
345730287 <345730287@C-GPXMMD2.tdsb.on.ca>
2026-05-14 09:38:54 -0400

    Completed Verdict class

---
+++ src/Verdict.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 345730287
+ */
+public class Verdict {
+    private String caseName;
+    private String studentVerdict;
+    private String reason;
+    
+    public Verdict() {
+        this.studentVerdict = "empty";
+    }
+    
+    public Verdict(String caseName, String verdict) {
+        this.caseName = caseName;
+        this.studentVerdict = studentVerdict;
+    }
+    
+    public void setCaseName() {
+        this.caseName = caseName;
+    }
+    
+    public String getCaseName() {
+        return caseName;
+    }
+    
+    public void setReason() {
+        this.reason = reason;
+    }
+    
+    public String getReason() {
+        return reason;
+    }
+    
+    public void setStudentVerdict() {
+        this.studentVerdict = studentVerdict;
+    }
+    
+    public String getStudentVerdict() {
+        return studentVerdict;
+    }
+    
+    public String toString() {
+        return caseName + studentVerdict + reason;
+    }
+    
+}

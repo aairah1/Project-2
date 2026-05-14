63abdb727a20ba430f8ecdb157720eeb71f9149e
345730287 <345730287@C-GPXMMD2.tdsb.on.ca>
2026-05-14 09:57:26 -0400

    Completed Misinformation and Intellectual Property classes

---
+++ src/IntellectualPropertyCase.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 345730287
+ */
+public class IntellectualPropertyCase extends EthicsCase{
+    private String ipType;
+    
+    public IntellectualPropertyCase (String caseTitle, String description, String ipType) {
+        super(caseTitle, description, "Intellectual Property");
+        this.ipType = ipType;
+    }
+    
+    public String getIpType() {
+        return ipType;
+    }
+    
+    public String toString() {
+        return super.toString() + "Intellectual Property: " + ipType;
+    }
+}
---
+++ src/MisinformationCase.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 345730287
+ */
+public class MisinformationCase extends EthicsCase{
+    private String mediaType;
+
+    public MisinformationCase(String caseTitle, String description, String mediaType) {
+        super (caseTitle, description, "Misinformation");
+        this.mediaType = mediaType;
+    }
+    
+    public String getMediaType() {
+        return mediaType;
+    }
+    
+    public String toString() {
+        return super.toString() + " Media Type: " + mediaType;
+    }    
+}

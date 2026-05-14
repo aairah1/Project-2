713be78e334f824bede5abee1681692f2472e81d
343079463 <343079463@C-FTFYJB2.tdsb.on.ca>
2026-05-14 10:03:26 -0400

    main class to test

---
+++ src/MyMain.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 343079463
+ */
+public class MyMain {
+    AlgorithmCase algo = new AlgorithmCase ( )
+}
